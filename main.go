ae6af930a81421b35f774228020381c42029a81f
Kyriakos Kentzoglanakis <kyriakos.kentzoglanakis@gmail.com>
2017-12-27 17:24:03 +0200

    Implements latency, status code and custom port

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func mockHandler(w http.ResponseWriter, r *http.Request, config *Config) {
+	time.Sleep(config.latency)
+	w.WriteHeader(config.responseCode)
 }
 
 func main() {
@@ -16,5 +20,5 @@ func main() {
 		mockHandler(w, r, config)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.port), nil))
 }
